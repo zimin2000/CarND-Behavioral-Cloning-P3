dfc62f5d93b1f031d031b3afdf9a3663742e96a8
Sergey Zimin <sergey.zimin@mail.ru>
2017-02-12 20:45:11 -0800

    First working version.

--- model.py
+++ model.py
@@ -1,17 +1,14 @@
-import sklearn
 import cv2
 import os
 import csv
-import pickle
 import tensorflow as tf
 import numpy as np
-from keras.layers import Input, Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D, Dropout, Activation
+from keras.layers import Input, Flatten, Dense, Lambda, Cropping2D, Convolution2D, MaxPooling2D, Dropout, Activation, SpatialDropout2D
 from keras.models import Model, Sequential
 from keras.models import load_model
 from sklearn.model_selection import train_test_split
 from sklearn.utils import shuffle
 
-DATA_DIR = "./data"
 BATCH_SIZE = 32
 
 IMG_WIDTH = 320
@@ -22,13 +19,51 @@ CROP_BOTTOM = 20
 CROP_SIDE = 1
 
 samples = []
-with open(os.path.join(DATA_DIR, 'driving_log.csv')) as csvfile:
-    reader = csv.reader(csvfile)
-    for line in reader:
-        if line[0] == 'center':
-            continue
 
-        samples.append(line)
+def filter_out_tiny(data):
+    """
+    Remove about 70% of steering values below 0.03
+    """
+
+    return filter((lambda item: abs(item['steering']) < 0.03 and np.random.randint(100) <= 70), data)
+
+def read_data(DATA_DIR, has_header=True):
+    """
+    Load CSV file into a list items. Each item is a dictionary or the name of 
+    column mapped to value. List of columns is expected to be the first line 
+    of the file.
+    """
+    data = []
+    columns = ["center","left","right","steering","throttle","brake","speed"]
+
+    with open(os.path.join(DATA_DIR, "driving_log.csv")) as FILE:
+        reader = csv.reader(FILE)
+
+        if has_header:
+            if reader.next() != columns:
+                raise Exception('Unexpected set of columns.')
+
+        for values in reader:
+            for i in range(3):
+                values[i] = os.path.join(DATA_DIR, 'IMG', values[i].split('/')[-1])
+
+            for i in range(3, 7):
+                values[i] = float(values[i])
+
+            data.append(dict(zip(columns, values)))
+
+    return data
+
+orig_samples = read_data("./data")
+extra_samples = read_data("./extra_data", has_header=False)
+
+samples = orig_samples + extra_samples
+
+print len(samples)
+
+samples = filter_out_tiny(samples)
+
+print len(samples)
 
 train_samples, validation_samples = train_test_split(samples, test_size=0.2)
 
@@ -45,16 +80,28 @@ def generator(samples, batch_size=BATCH_SIZE):
             angles = []
 
             for sample in batch_samples:
-                name = os.path.join(DATA_DIR, 'IMG', sample[0].split('/')[-1])
+                choice = np.random.randint(100)
 
-                center_image = cv2.imread(name)
+                if choice < 50:
+                    image_name = sample['center']
+                    angle = sample['steering']
 
-                center_angle = float(sample[3])
+                elif choice < 75:
+                    image_name = sample['left']
+                    angle = sample['steering'] + 0.20
 
-#                print "{} {} {}".format(name, center_image.shape, center_angle)
+                else:
+                    image_name = sample['right']
+                    angle = sample['steering'] - 0.20
 
-                images.append(center_image)
-                angles.append(center_angle)
+                image = cv2.imread(image_name)
+
+                # print "{} {} {}".format(name, center_image.shape, center_angle)
+
+                
+
+                images.append(image)
+                angles.append(angle)
 
             # trim image to only see section with road
             X_train = np.array(images)
@@ -62,95 +109,96 @@ def generator(samples, batch_size=BATCH_SIZE):
 
             yield (X_train, y_train)
 
-def train():
-    # compile and train the model using the generator function
-    train_generator = generator(train_samples, batch_size = BATCH_SIZE)
-    validation_generator = generator(validation_samples, batch_size = BATCH_SIZE)
 
+
+def build_model(input_shape):
     model = Sequential()
 
     # Preprocessing 0.1: Cropping useless top and bottom pixels
-    # 320 x 160 x 3 -> 3 x 320 x 90
-    model.add(Cropping2D(cropping=((CROP_TOP, CROP_BOTTOM), (CROP_SIDE, CROP_SIDE)), input_shape=(IMG_HEIGHT, IMG_WIDTH, 3), dim_ordering='tf'))
+    # 3 @ 320 x 160 -> 3 @ 318 x 90
+    model.add(Cropping2D(cropping=((CROP_TOP, CROP_BOTTOM), (CROP_SIDE, CROP_SIDE)), input_shape=input_shape, dim_ordering='tf'))
 
     # Preprocessing 0.2: Centered around zero with small standard deviation
-    # 320 x 90 x 3 -> 320 x 90 x 3
-#    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
+    # 3 @ 318 x 90 -> 3 @ 318 x 90
+    model.add(Lambda(lambda x: (x / 255.0) - 0.5))
 
-    # Layer 1:
-    # 3 x 318 x 90 -> Conv 7 x 7 (+1 x +1) -> MaxPool 3 x 3 -> Dropout -> Relu -> 24 x 104 x 28
+
+    # Convolutional Layer 1:
+    # 3 @ 318 x 90 -> Conv 7 x 7 (+1 x +1) -> MaxPool 3 x 3 -> Dropout -> Elu -> 24 @ 104 x 28
     model.add(Convolution2D(24, 7, 7, border_mode='valid'))
     model.add(MaxPooling2D((3, 3)))
     model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
-
-    # Layer 2:
-    # 24 x 104 x 28 -> Conv 5 x 5 (+1 x +1) -> MaxPool 2 x 2 -> Dropout -> Relu -> 36 x 50 x 12
+    # Convolutional Layer 2:
+    # 24 @ 104 x 28 -> Conv 5 x 5 (+1 x +1) -> MaxPool 2 x 2 -> Dropout -> Elu -> 36 @ 50 x 12
     model.add(Convolution2D(36, 5, 5, border_mode='valid'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
-
-    # Layer 3:
-    # 36 x 50 x 12 -> Conv 5 x 5 (+1 x +1) -> MaxPool 2 x 2 -> Dropout -> Relu -> 48 x 23 x 4
+    # Convolutional Layer 3:
+    # 36 @ 50 x 12 -> Conv 5 x 5 (+1 x +1) -> MaxPool 2 x 2 -> Dropout -> Elu -> 48 @ 23 x 4
     model.add(Convolution2D(48, 5, 5, border_mode='valid'))
     model.add(MaxPooling2D((2, 2)))
     model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
-
-    # Layer 4:
-    # 48 x 23 x 4 -> Conv 3 x 3 -> Dropout -> Relu -> 64 x 21 x 2
+    # Convolutional Layer 4:
+    # 48 @ 23 x 4 -> Conv 3 x 3 -> Dropout -> Elu -> 64 @ 21 x 2
     model.add(Convolution2D(64, 3, 3, border_mode='valid'))
     model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
-
-    # Layer 5:
-    # 64 x 21 x 2 -> Conv 3 x 2 -> Dropout -> Relu -> 64 x 19 x 1
+    # Convolutional Layer 5:
+    # 64 @ 21 x 2 -> Conv 3 x 2 -> Dropout -> Elu -> 64 @ 19 x 1
     model.add(Convolution2D(64, 2, 3, border_mode='valid'))
     model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
 
     # FC Layer 1:
-    # 64 x 19 x 1 -> Dense -> 1164
+    # 64 @ 19 x 1 -> Flatten -> Dropout -> Elu -> ????
     model.add(Flatten())
-
-    model.add(Dense(1164))
-    model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Dropout(0.5))
+    model.add(Activation('elu'))
     
     # FC Layer 2:
-    # 1164 -> Dense -> 100
+    # 1164 -> Dropout -> Elu -> 100
     model.add(Dense(100))
-    model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
     
     # FC Layer 3:
-    # 100 -> Dense -> 50
+    # 100 -> Dropout -> Elu -> 50
     model.add(Dense(50))
-    model.add(Dropout(0.95))
-    model.add(Activation('relu'))
+    model.add(Activation('elu'))
     
     # FC Layer 4:
-    # 50 -> Dense -> 10
+    # 50 -> Dropout -> 10
     model.add(Dense(10))
-    model.add(Dropout(0.95))
-    model.add(Activation('relu'))
-    
+    model.add(Activation('elu'))
+
+    model.add(Dropout(0.5))
+
     # Output Layer:
     # 10 -> Dense -> 1
     model.add(Dense(1))
 
+    return model
+    
+
+def train():
+
+    model = build_model((IMG_HEIGHT, IMG_WIDTH, 3))
+
+    model.summary()
+
     model.compile(loss='mse', optimizer='adam')
 
-    model.fit_generator(train_generator, 
-                        samples_per_epoch = len(train_samples), 
-                        validation_data = validation_generator, 
-                        nb_val_samples = len(validation_samples), 
-                        nb_epoch = 10)
+    model.fit_generator(generator = generator(train_samples, batch_size = BATCH_SIZE), 
+                        samples_per_epoch = len(train_samples) * 4, 
+                        validation_data = generator(validation_samples, batch_size = BATCH_SIZE), 
+                        nb_val_samples = len(validation_samples) * 4, 
+                        nb_epoch = 50)
 
     model.save('model.h5')
 
